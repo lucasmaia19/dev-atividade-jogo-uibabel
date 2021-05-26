32400d7c47dc0ff8e310a043f950adf73599a880
lucasmaia19 <lucasmaiaalves19@gmail.com>
2021-05-26 14:47:32 -0300

    adicionado grid atividade-montar

---
+++ react-animation/react-animation/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import { Example } from "./Example";
+import Example2 from "./Example2";
+import Example3 from "./Example3";
+import Example4 from "./Example4";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <Example />
+    {/* <Example2 /> */}
+    {/* <Example3 /> */}
+    {/* <Example4 /> */}
+    {/* <App /> */}
+  </React.StrictMode>,
+  rootElement
+);
